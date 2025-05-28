f29bdfe91a936ba92ea45089bcea37b0753b7ef9
BerndderBernd <haha_hehe@live.de>
2025-05-28 20:51:58 +0200

    Delete pages directory

--- pages/admin.js
+++
@@ -1,7 +0,0 @@
-import { useState } from 'react';
-export default function Admin() {
-  const [pw, setPw] = useState('');
-  return pw === 'magie2025' ? <div>Adminbereich</div> : (
-    <input placeholder="Passwort" onChange={e => setPw(e.target.value)} />
-  );
-}
