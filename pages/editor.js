537a5078ab20cc9285266aa46b219c12d0af03bd
BerndderBernd <haha_hehe@live.de>
2025-05-28 14:08:12 +0200

    Add files via upload

---
+++ pages/editor.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import PagePair from '../components/PagePair';
+export default function Editor() {
+  return (
+    <div>
+      <h1>Bilderbuch-Magie</h1>
+      <PagePair />
+    </div>
+  );
+}
