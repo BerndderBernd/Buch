5fe82eeb80e346bd853e7d39b4d091be37d695e2
BerndderBernd <haha_hehe@live.de>
2025-05-28 20:52:15 +0200

    Delete utils directory

--- utils/pdf.js
+++
@@ -1,5 +0,0 @@
-import html2pdf from 'html2pdf.js';
-export function exportToPDF(elementId) {
-  const element = document.getElementById(elementId);
-  html2pdf().from(element).save('bilderbuch.pdf');
-}
